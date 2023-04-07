072fceb3a20a92ab487379d2c5892ca519691d34
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-04-07 09:22:14 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
+++ 프로그래머스/lv0/120583. 중복된 숫자 개수/중복된 숫자 개수.py
@@ -2,3 +2,7 @@ from collections import Counter
 
 def solution(array, n):
     return Counter(array).get(n)
+
+def solution(array, n):
+    value = Counter(array).get(n)
+    return 0 if value == None else value
