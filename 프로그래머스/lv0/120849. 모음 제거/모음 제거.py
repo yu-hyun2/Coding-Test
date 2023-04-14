b3700360832a69766ef65b36d782ed4cbe13945c
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-04-14 09:25:43 +0900

    [level 0] Title: 모음 제거, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -1,4 +1,15 @@
+# #1
+# import re
+
+# def solution(my_string):
+#     return re.sub(r'[aeiou]', '', my_string)
+
+# #2
+# def solution(my_string):
+#     return my_string.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')
+
+#3 정규표현식
 import re
 
 def solution(my_string):
-    return re.sub(r'[aeiou]', '', my_string)
+    return re.sub(r'[aeiou]','', my_string)
