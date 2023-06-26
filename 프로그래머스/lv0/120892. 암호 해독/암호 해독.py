e7373e370a286aaac56f08db6c6e11ecd4fc97fa
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-06-26 23:26:11 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.py
@@ -0,0 +1,10 @@
+def solution(cipher, code):
+    answer = ''
+
+    for i in range(0, len(cipher)):
+        if (i+1) % code == 0:
+            answer += cipher[i]
+        else:
+            continue
+            
+    return answer
