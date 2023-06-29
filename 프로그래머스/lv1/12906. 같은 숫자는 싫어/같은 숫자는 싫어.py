1244f3739c361f5f98aae9eee6b447e9122faad3
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-06-29 16:17:47 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 124.48 ms, Memory: 28 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -0,0 +1,10 @@
+def solution(arr):
+
+    answer = []
+    for i in range(len(arr)):
+        if i == 0 or arr[i] != arr[i-1]:
+            answer.append(arr[i])
+        else:
+            continue
+
+    return answer
