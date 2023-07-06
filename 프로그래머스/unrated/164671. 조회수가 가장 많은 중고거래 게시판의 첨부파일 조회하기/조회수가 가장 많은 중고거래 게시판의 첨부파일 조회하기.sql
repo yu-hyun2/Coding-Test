eaf6bbaa7ed935d461ddda86cae7c82a85161231
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-07-06 21:04:33 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,16 @@
+# 조회수가 가장 많은 게시판
+# SELECT BOARD_ID, VIEWS
+# FROM USED_GOODS_BOARD
+# ORDER BY VIEWS DESC LIMIT 1
+
+SELECT CONCAT('/home/grep/src/',
+              BOARD_ID,
+              '/',
+              FILE_ID, 
+              FILE_NAME, 
+              FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_FILE 
+WHERE BOARD_ID = (SELECT BOARD_ID
+                FROM USED_GOODS_BOARD
+                ORDER BY VIEWS DESC LIMIT 1)
+ORDER BY FILE_ID DESC
