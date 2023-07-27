c76932acf3d1ff467dc7e5623b7917abbf01967f
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-07-27 12:10:44 +0900

    [Bronze V] Title: 시험 성적, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+s = int(input())
+if 90 <= s <= 100:
+    print('A')
+elif 80 <= s <= 89:
+    print('B')
+elif 70 <= s <= 79:
+    print('C')
+elif 60 <= s <= 69:
+    print('D')
+else:
+    print('F')
