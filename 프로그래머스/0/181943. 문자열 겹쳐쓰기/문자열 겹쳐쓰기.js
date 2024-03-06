510b1a2afdcbc25a30a0876de28abf6467a962bf
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2024-03-06 22:11:02 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -2,11 +2,14 @@ function solution(my_string, overwrite_string, s) {
   var answer = '';
 
   for (var i = 0; i < my_string.length; i++) {
-    answer +=
-      (i >= s) & (i - s < overwrite_string.length)
-        ? overwrite_string[i - s]
-        : my_string[i];
-  }
-
+      
+    if (i >= s & i - s < overwrite_string.length) {
+        answer += overwrite_string[i-s]
+    }
+    else {
+        answer += my_string[i];
+    }
+}
   return answer;
+
 }
