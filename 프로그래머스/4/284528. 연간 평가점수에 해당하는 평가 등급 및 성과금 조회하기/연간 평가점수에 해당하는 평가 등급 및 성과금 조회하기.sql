5d14a23f27adec87af0fb20bddcb35f92b5ff620
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2024-05-05 23:11:36 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -0,0 +1,20 @@
+SELECT G.EMP_NO, E.EMP_NAME,
+    (CASE 
+        WHEN AVG(G.SCORE) >= 96 THEN 'S' 
+        WHEN AVG(G.SCORE) >= 90 THEN 'A' 
+        WHEN AVG(G.SCORE) >= 80 THEN 'B'
+        ELSE 'C'
+     END
+    ) AS GRADE,
+    
+    (CASE 
+        WHEN AVG(G.SCORE) >= 96 THEN E.SAL * 0.2
+        WHEN AVG(G.SCORE) >= 90 THEN E.SAL * 0.15 
+        WHEN AVG(G.SCORE) >= 80 THEN E.SAL * 0.1
+        ELSE 0
+     END
+    ) AS BONUS
+
+FROM HR_GRADE G JOIN HR_EMPLOYEES E ON G.EMP_NO = E.EMP_NO
+GROUP BY G.EMP_NO
+ORDER BY G.EMP_NO
